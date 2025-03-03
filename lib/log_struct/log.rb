927c81335d3e0bbc67ef74266731da5e82afbb52
Nathan Broadbent <git@ndbroadbent.com>
2025-03-03 13:27:15 +1300

    log

--- lib/log_struct/log.rb
+++ lib/log_struct/log.rb
@@ -1,10 +1,15 @@
 # typed: strict
 # frozen_string_literal: true
 
+# Enums
 require_relative "log_source"
 require_relative "log_event"
-require_relative "log_violation_type"
+require_relative "log_security_event"
+
+# Log Structs
 require_relative "log/log_interface"
+require_relative "log/request_interface"
+require_relative "log/log_serialization"
 require_relative "log/error"
 require_relative "log/exception"
 require_relative "log/email"
