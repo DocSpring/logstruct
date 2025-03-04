c4fb6ba18d7f524f050d5fa715cbbe8bc38a1b16
Nathan Broadbent <git@ndbroadbent.com>
2025-03-05 08:14:28 +1300

    test config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -58,7 +58,22 @@ require "log_struct"
 
 # Configure LogStruct
 LogStruct.configure do |config|
-  # change any defaults if needed
+  config.enabled = true
+  
+  # Configure integrations
+  config.integrations.enable_lograge = true
+  config.integrations.enable_actionmailer = true
+  config.integrations.enable_activejob = true
+  config.integrations.enable_sidekiq = true
+  config.integrations.enable_shrine = true  
+  config.integrations.enable_active_storage = true
+  config.integrations.enable_carrierwave = true
+  config.integrations.enable_rack_error_handler = true
+  config.integrations.enable_host_authorization = true
+  
+  # Configure filters
+  config.filters.hash_salt = "test"
+  config.filters.hash_length = 12
 end
 
 # Load all test support files
