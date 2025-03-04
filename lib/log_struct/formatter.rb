6cc9a4ce54b2d14845a8a66e6c9db90a4e3e6d15
Nathan Broadbent <git@ndbroadbent.com>
2025-03-05 11:02:06 +1300

    severity is only integer

--- lib/log_struct/formatter.rb
+++ lib/log_struct/formatter.rb
@@ -145,10 +145,8 @@ module LogStruct
     end
 
     # Serializes Log (or string) into JSON
-    sig { params(severity: T.any(String, Integer), time: Time, progname: T.nilable(String), log_value: T.untyped).returns(String) }
+    sig { params(severity: Integer, time: Time, progname: T.nilable(String), log_value: T.untyped).returns(String) }
     def call(severity, time, progname, log_value)
-      # Convert severity to string if it's an integer (for compatibility with tests)
-      severity = severity.to_s if severity.is_a?(Integer)
       data = log_value_to_hash(log_value, time: time)
 
       # Filter params, scrub sensitive values, format ActiveJob GlobalID arguments
