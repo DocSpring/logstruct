6d18fb2dc4b40617ee89116598bea8fc28bb4e88
Nathan Broadbent <git@ndbroadbent.com>
2025-03-05 12:17:05 +1300

    fix

--- test/support/filter_by_line.rb
+++ test/support/filter_by_line.rb
@@ -16,21 +16,19 @@ module Minitest
 
         # Get the target line from the environment
         line = ENV["LINE"]&.to_i
-        if line && name.include?("Test")
-          methods = methods.select do |method_name|
-            method = instance_method(method_name)
-            source_location = method.source_location
-            # If source location doesn't contain line info, include the method
-            next true unless source_location
+        return methods unless line && name&.include?("Test")
 
-            # Include this method if it's defined at or before the target line
-            # and the next method is defined after
-            _, method_line = source_location
-            method_line <= line
-          end
+        methods.select do |method_name|
+          method = instance_method(method_name)
+          source_location = method.source_location
+          # If source location doesn't contain line info, include the method
+          next true unless source_location
+
+          # Include this method if it's defined at or before the target line
+          # and the next method is defined after
+          _, method_line = source_location
+          method_line <= line
         end
-
-        methods
       end
     end
   end
