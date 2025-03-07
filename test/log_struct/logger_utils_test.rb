0d90a81584f121b2ba620531f90125e82f573a92
Nathan Broadbent <git@ndbroadbent.com>
2025-03-07 14:36:57 +1300

    logger utils done

--- test/log_struct/logger_utils_test.rb
+++ test/log_struct/logger_utils_test.rb
@@ -11,7 +11,7 @@ module LogStruct
       @original_rails_env = ::Rails.env
       @original_rails_root = ::Rails.root
       @original_rails_log_to_stdout = ENV["RAILS_LOG_TO_STDOUT"]
-      
+
       # Create a test logger for testing
       @test_logger = ::Logger.new($stdout)
     end
@@ -26,9 +26,9 @@ module LogStruct
     def test_determine_log_target_with_original_logger
       test_io = StringIO.new
       logger = ::Logger.new(test_io)
-      
+
       target = LoggerUtils.determine_log_target(logger)
-      
+
       assert_equal test_io, target
     end
 
@@ -36,9 +36,9 @@ module LogStruct
       test_io = StringIO.new
       rails_logger = ::Logger.new(test_io)
       ::Rails.logger = rails_logger
-      
+
       target = LoggerUtils.determine_log_target(nil)
-      
+
       assert_equal test_io, target
     end
 
@@ -46,19 +46,19 @@ module LogStruct
       # Create a mock Rails logger with no logdev
       # First store original logdev
       original_logdev = ::Rails.logger.instance_variable_get(:@logdev)
-      
+
       # Set up Rails logger to have nil logdev
       rails_logger = ::Logger.new(StringIO.new)
       rails_logger.remove_instance_variable(:@logdev) if rails_logger.instance_variable_defined?(:@logdev)
       ::Rails.logger = rails_logger
-      
+
       # Set the RAILS_LOG_TO_STDOUT env var
       ENV["RAILS_LOG_TO_STDOUT"] = "1"
-      
+
       target = LoggerUtils.determine_log_target(nil)
-      
+
       assert_equal $stdout, target
-      
+
       # Restore original logdev
       ::Rails.logger.instance_variable_set(:@logdev, original_logdev) if original_logdev
     end
@@ -68,18 +68,19 @@ module LogStruct
       rails_logger = ::Logger.new(StringIO.new)
       rails_logger.remove_instance_variable(:@logdev) if rails_logger.instance_variable_defined?(:@logdev)
       ::Rails.logger = rails_logger
-      
+
       # Remove the STDOUT env var
       ENV.delete("RAILS_LOG_TO_STDOUT")
-      
+
       # Set up test environment
       ::Rails.stub(:env, ActiveSupport::StringInquirer.new("test")) do
         # Set up Rails.root to return a predictable path
         test_path = File.expand_path("../../tmp/test_root", __dir__)
         path_obj = Pathname.new(test_path)
-        
+
         ::Rails.stub(:root, path_obj) do
           target = LoggerUtils.determine_log_target(nil)
+
           assert_equal "#{test_path}/log/test.log", target
         end
       end
@@ -90,13 +91,14 @@ module LogStruct
       rails_logger = ::Logger.new(StringIO.new)
       rails_logger.remove_instance_variable(:@logdev) if rails_logger.instance_variable_defined?(:@logdev)
       ::Rails.logger = rails_logger
-      
+
       # Remove the STDOUT env var
       ENV.delete("RAILS_LOG_TO_STDOUT")
-      
+
       # Set up non-test environment
       ::Rails.stub(:env, ActiveSupport::StringInquirer.new("development")) do
         target = LoggerUtils.determine_log_target(nil)
+
         assert_equal $stdout, target
       end
     end
@@ -107,35 +109,35 @@ module LogStruct
       # Set up logger with known level
       test_logger = ::Logger.new(StringIO.new)
       test_logger.level = ::Logger::DEBUG
-      
+
       level = LoggerUtils.determine_log_level(test_logger)
-      
+
       assert_equal ::Logger::DEBUG, level
     end
 
     def test_determine_log_level_with_rails_logger
       # Set up Rails logger with known level
       ::Rails.logger.level = ::Logger::WARN
-      
+
       level = LoggerUtils.determine_log_level(nil)
-      
+
       assert_equal ::Logger::WARN, level
     end
 
     def test_determine_log_level_with_unresponsive_logger
       # We need to use a properly typed logger but mock it to be unresponsive
       unresponsive_logger = ::Logger.new(StringIO.new)
-      
+
       # Set up Rails logger with known level
       ::Rails.logger.level = ::Logger::INFO
-      
+
       # Mock the respond_to? method to return false for :level
       unresponsive_logger.define_singleton_method(:respond_to?) do |method_name|
         method_name != :level
       end
-      
+
       level = LoggerUtils.determine_log_level(unresponsive_logger)
-      
+
       assert_equal ::Logger::INFO, level
     end
 
@@ -144,19 +146,19 @@ module LogStruct
     def test_create_logger_with_default_options
       test_io = StringIO.new
       test_level = ::Logger::ERROR
-      
+
       # Set up Rails logger to use our test IO with a known level
       ::Rails.logger = ::Logger.new(test_io)
       ::Rails.logger.level = test_level
-      
+
       # We'll use the real LogStruct::Logger class but stub the new method
       # to verify what parameters it receives
       real_new = LogStruct::Logger.method(:new)
-      
+
       begin
-        device_received = nil
-        level_received = nil
-        
+        device_received = T.let(nil, T.nilable(String))
+        level_received = T.let(nil, T.nilable(Integer))
+
         LogStruct::Logger.define_singleton_method(:new) do |device, shift_age = 0, shift_size = 1048576, level: nil|
           device_received = device
           level_received = level
@@ -167,9 +169,9 @@ module LogStruct
           logger.instance_variable_set(:@logdev, ::Logger::LogDevice.new(StringIO.new))
           logger
         end
-        
-        result = LoggerUtils.create_logger(LogStruct::Logger)
-        
+
+        LoggerUtils.create_logger(LogStruct::Logger)
+
         # Verify params
         assert_equal test_io, device_received
         assert_equal test_level, level_received
@@ -184,14 +186,14 @@ module LogStruct
       custom_io = StringIO.new
       custom_level = ::Logger::INFO
       options = {logdev: custom_io, level: custom_level}
-      
+
       # We'll use the real LogStruct::Logger class but stub the new method
       real_new = LogStruct::Logger.method(:new)
-      
+
       begin
-        device_received = nil
-        level_received = nil
-        
+        device_received = T.let(nil, T.nilable(String))
+        level_received = T.let(nil, T.nilable(Integer))
+
         LogStruct::Logger.define_singleton_method(:new) do |device, shift_age = 0, shift_size = 1048576, level: nil|
           device_received = device
           level_received = level
@@ -202,11 +204,11 @@ module LogStruct
           logger.instance_variable_set(:@logdev, ::Logger::LogDevice.new(StringIO.new))
           logger
         end
-        
+
         # We intentionally don't stub determine_* methods
         # to verify they don't get called with explicit options
-        result = LoggerUtils.create_logger(LogStruct::Logger, options: options)
-        
+        LoggerUtils.create_logger(LogStruct::Logger, options: options)
+
         # Verify params
         assert_equal custom_io, device_received
         assert_equal custom_level, level_received
@@ -222,17 +224,17 @@ module LogStruct
       original_io = StringIO.new
       original_level = ::Logger::WARN
       original_logger = LogStruct::Logger.new(original_io, level: original_level)
-      
+
       # Stub the determine methods to control their return values
       LoggerUtils.stub(:determine_log_target, original_io) do
         LoggerUtils.stub(:determine_log_level, original_level) do
           # We'll use the real LogStruct::Logger class but stub the new method
           real_new = LogStruct::Logger.method(:new)
-          
+
           begin
-            device_received = nil
-            level_received = nil
-            
+            device_received = T.let(nil, T.nilable(String))
+            level_received = T.let(nil, T.nilable(Integer))
+
             LogStruct::Logger.define_singleton_method(:new) do |device, shift_age = 0, shift_size = 1048576, level: nil|
               device_received = device
               level_received = level
@@ -243,10 +245,10 @@ module LogStruct
               logger.instance_variable_set(:@logdev, ::Logger::LogDevice.new(StringIO.new))
               logger
             end
-            
+
             # Pass the original_logger to test this code path
-            result = LoggerUtils.create_logger(LogStruct::Logger, original_logger: original_logger)
-            
+            LoggerUtils.create_logger(LogStruct::Logger, original_logger: original_logger)
+
             # Verify params
             assert_equal original_io, device_received
             assert_equal original_level, level_received
@@ -258,22 +260,22 @@ module LogStruct
         end
       end
     end
-    
+
     def test_create_logger_with_partial_options
       # Custom options with only level provided
       custom_level = ::Logger::INFO
       test_io = StringIO.new
       options = {level: custom_level}
-      
+
       # Stub determine_log_target to return a known value
       LoggerUtils.stub(:determine_log_target, test_io) do
         # We'll use the real LogStruct::Logger class but stub the new method
         real_new = LogStruct::Logger.method(:new)
-        
+
         begin
-          device_received = nil
-          level_received = nil
-          
+          device_received = T.let(nil, T.nilable(String))
+          level_received = T.let(nil, T.nilable(Integer))
+
           LogStruct::Logger.define_singleton_method(:new) do |device, shift_age = 0, shift_size = 1048576, level: nil|
             device_received = device
             level_received = level
@@ -284,10 +286,10 @@ module LogStruct
             logger.instance_variable_set(:@logdev, ::Logger::LogDevice.new(StringIO.new))
             logger
           end
-          
+
           # Use partial options to test this code path
-          result = LoggerUtils.create_logger(LogStruct::Logger, options: options)
-          
+          LoggerUtils.create_logger(LogStruct::Logger, options: options)
+
           # Verify params
           assert_equal test_io, device_received
           assert_equal custom_level, level_received
