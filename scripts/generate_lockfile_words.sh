b7b39434f14bd55f1a1b2e723ba0b697890536b2
Nathan Broadbent <git@ndbroadbent.com>
2025-09-04 13:35:44 +1200

    TODO - add pnpm-lock.yaml support to cspell-lockfile-dicts

--- scripts/generate_lockfile_words.sh
+++ scripts/generate_lockfile_words.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# TODO - add pnpm-lock.yaml support to cspell-lockfile-dicts
+
 echo "Generating lockfile words for root"
 npx @docspring/cspell-lockfile-dicts \
   --path .cspell/generated-lockfile-words-root.txt \
-  --lockfiles Gemfile.lock package-lock.json
+  --lockfiles Gemfile.lock pnpm-lock.yaml
 
-echo "Generating lockfile words for site"
-npx @docspring/cspell-lockfile-dicts \
-  --path .cspell/generated-lockfile-words-site.txt \
-  --lockfiles site/package-lock.json
+# echo "Generating lockfile words for site"
+# npx @docspring/cspell-lockfile-dicts \
+#   --path .cspell/generated-lockfile-words-site.txt \
+#   --lockfiles site/pnpm-lock.yaml
