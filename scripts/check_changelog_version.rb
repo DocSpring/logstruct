d6d668b6e4c241c394a8cf7e464796aa6a707d47
Nathan Broadbent <git@ndbroadbent.com>
2025-09-05 20:11:40 +1200

    don't check changelog entries for rc versions

--- scripts/check_changelog_version.rb
+++ scripts/check_changelog_version.rb
@@ -7,13 +7,19 @@ require "date"
 version = File.read("lib/log_struct/version.rb")[/VERSION\s*=\s*"([^"]+)"/, 1]
 abort "Could not determine version from lib/log_struct/version.rb" unless version
 
+# Skip changelog requirement for RC builds (e.g., 0.0.2-rc1 or 0.0.2.rc1)
+if version.match?(/(?:-|\.)rc\d*$/i)
+  puts "RC version detected (#{version}); skipping CHANGELOG check."
+  exit 0
+end
+
 changelog = begin
   File.read("CHANGELOG.md")
 rescue
   ""
 end
 
-# Accept headings like: ## [0.0.2-rc1] - 2025-09-05 or ## 0.0.2-rc1
+# Accept headings like: ## [0.0.2] - 2025-09-05 or ## 0.0.2
 heading_regex = /^##\s*\[?#{Regexp.escape(version)}\]?/m
 
 if changelog.match?(heading_regex)
